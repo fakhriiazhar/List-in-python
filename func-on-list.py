2684b43009ba91fa0004d544a1b3c128bc6eb2c1
Fakhrii Azhar <154650463+fakhriiazhar@users.noreply.github.com>
2024-10-19 12:12:30 +0800

    Create func-on-list.py

---
+++ func-on-list.py
@@ -0,0 +1,15 @@
+#fungsi pada list
+nama = ("Fakhrii", "Fakhrii Azhar", "anda")
+nim = (23, 11, 10, 23, 41, 16, 9)
+lenght = len(nama)
+print(lenght)
+
+nama = ("fakhrii", "Fakhrii Azhar", "anda")
+nim = (23, 11, 10, 24, 41, 16, 9)
+minimum_value = min(nim)
+print("nilai terkecil dalam list adalah:", minimum_value)
+
+nama = ("Fakhrii", "Fakhrii azhar", "anda")
+nim = (23, 11, 10, 24, 41, 16, 9)
+maximum_value = max(nim)
+print("nilai tertinggi dalam list adalah:", maximum_value)

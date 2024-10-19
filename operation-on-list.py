12b33bee60b801733ec29e993ece2aa42e2af189
Fakhrii Azhar <154650463+fakhriiazhar@users.noreply.github.com>
2024-10-19 12:11:54 +0800

    Create operation-on-list.py

---
+++ operation-on-list.py
@@ -0,0 +1,13 @@
+#operation on list
+nama = ["Fakhrii", "Fakhrii Azhar", "anda"]
+nim = [23, 11, 10 ,24, 41, 16, 9]
+matakuliah = ["praktikum dasar", "pemrograman"]
+
+nama[0] = "fahri"
+print(nama)
+
+del nim[6]
+print(nim)
+
+del matakuliah[1] 
+print(matakuliah)
